a4e38c3d2fea59d4185fac48f253e1a19774c02f
adawolfs <adawolfs@gmail.com>
2022-06-07 22:50:06 -0600

    implement controllers on mobile

--- lib/src/games/tetris/ui.dart
+++ lib/src/games/tetris/ui.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:interactive_flutter/src/games/tetris/bloc.dart';
+import 'package:interactive_flutter/src/ui/extensions.dart';
 
 class TetrisUI extends StatelessWidget {
   const TetrisUI({Key? key}) : super(key: key);
@@ -22,50 +23,103 @@ class TetrisUI extends StatelessWidget {
                     onTap: () {
                       BlocProvider.of<TetrisBloc>(context).add(TetrisRestart());
                     },
-                    child: Icon(size: 50.0, Icons.refresh)),
+                    child: const Icon(size: 50.0, Icons.refresh)),
               ],
             ))
-          : RawKeyboardListener(
-              focusNode: focusNode,
-              onKey: (event) {
-                if (event.isKeyPressed(LogicalKeyboardKey.arrowLeft)) {
-                  BlocProvider.of<TetrisBloc>(context).add(TetrisMoveLeft());
-                } else if (event.isKeyPressed(LogicalKeyboardKey.arrowRight)) {
-                  BlocProvider.of<TetrisBloc>(context).add(TetrisMoveRight());
-                } else if (event.isKeyPressed(LogicalKeyboardKey.arrowDown)) {
-                  BlocProvider.of<TetrisBloc>(context).add(TetrisMoveDown());
-                } else if (event.isKeyPressed(LogicalKeyboardKey.space)) {
-                  BlocProvider.of<TetrisBloc>(context).add(TetrisRotate());
-                }
-              },
-              child: Container(
-                color: Colors.grey,
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: state.board
-                        .map(
-                          (e) => Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: e
-                                .map(
-                                  (e) => Padding(
-                                    padding: const EdgeInsets.all(1.0),
-                                    child: Container(
-                                      color: e ? Colors.black : Colors.white,
-                                      height: 20,
-                                      width: 20,
-                                    ),
+          : LayoutBuilder(builder: (context, constraints) {
+              return RawKeyboardListener(
+                focusNode: focusNode,
+                onKey: (event) {
+                  if (event.isKeyPressed(LogicalKeyboardKey.arrowLeft)) {
+                    BlocProvider.of<TetrisBloc>(context).add(TetrisMoveLeft());
+                  } else if (event
+                      .isKeyPressed(LogicalKeyboardKey.arrowRight)) {
+                    BlocProvider.of<TetrisBloc>(context).add(TetrisMoveRight());
+                  } else if (event.isKeyPressed(LogicalKeyboardKey.arrowDown)) {
+                    BlocProvider.of<TetrisBloc>(context).add(TetrisMoveDown());
+                  } else if (event.isKeyPressed(LogicalKeyboardKey.space)) {
+                    BlocProvider.of<TetrisBloc>(context).add(TetrisRotate());
+                  }
+                },
+                child: Container(
+                  color: Colors.grey,
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: state.board
+                              .map(
+                                (e) => Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: e
+                                      .map(
+                                        (e) => Padding(
+                                          padding: const EdgeInsets.all(1.0),
+                                          child: Container(
+                                            color:
+                                                e ? Colors.black : Colors.white,
+                                            height: 20,
+                                            width: 20,
+                                          ),
+                                        ),
+                                      )
+                                      .toList(),
+                                ),
+                              )
+                              .toList(),
+                        ),
+                        constraints.isMobile
+                            ? Column(
+                                children: [
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      GestureDetector(
+                                        onTap: () {
+                                          BlocProvider.of<TetrisBloc>(context)
+                                              .add(TetrisMoveLeft());
+                                        },
+                                        child: const Icon(
+                                            size: 50.0, Icons.arrow_left),
+                                      ),
+                                      GestureDetector(
+                                        onTap: () {
+                                          BlocProvider.of<TetrisBloc>(context)
+                                              .add(TetrisRotate());
+                                        },
+                                        child: const Icon(
+                                            size: 50.0,
+                                            Icons.rotate_90_degrees_cw),
+                                      ),
+                                      GestureDetector(
+                                        onTap: () {
+                                          BlocProvider.of<TetrisBloc>(context)
+                                              .add(TetrisMoveRight());
+                                        },
+                                        child: const Icon(
+                                            size: 50.0, Icons.arrow_right),
+                                      ),
+                                    ],
                                   ),
-                                )
-                                .toList(),
-                          ),
-                        )
-                        .toList(),
+                                  GestureDetector(
+                                    onTap: () {
+                                      BlocProvider.of<TetrisBloc>(context)
+                                          .add(TetrisMoveDown());
+                                    },
+                                    child: const Icon(
+                                        size: 50.0, Icons.arrow_drop_down),
+                                  )
+                                ],
+                              )
+                            : Container()
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            );
+              );
+            });
     });
   }
 }
